22517bc59dd3b9d5824eccd136c029940cf2f97b
Felipe Angelo <fangeloferreira2013@gmail.com>
2020-12-23 11:59:20 -0300

    23/12/2020-11:59-config1
    config1

--- js/about.01a61f91.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"20ce":function(t,e,c){"use strict";c("58ce")},"58ce":function(t,e,c){},f820:function(t,e,c){"use strict";c.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,c=t._self._c||e;return c("div",{staticClass:"body"},[c("div",{staticClass:"about"},[c("h1",[t._v("This is an about page")])])])}],a=(c("20ce"),c("2877")),i={},u=Object(a["a"])(i,n,s,!1,null,"247c1a0c",null);e["default"]=u.exports}}]);
-//# sourceMappingURL=about.01a61f91.js.map
