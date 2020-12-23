967111a82f40b9bc52cb773a200e37f1f2424fac
Felipe Angelo <fangeloferreira2013@gmail.com>
2020-12-23 12:13:21 -0300

    23/12/2020-12:12-Reconstructing Repository
    Reconstructing Repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# An test with Vue.js
+link to site:
+https://epilef1177.github.io/vue-teste/
---
+++ src/views/Photos.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="body">
+        <section>
+            <img src="../assets/photos/blackbird.jpg" alt="">
+        </section>
+        <section>
+            <img src="../assets/photos/dolphin.jpg" alt="">
+        </section>
+        <section>
+            <img src="../assets/photos/fallow-deer.jpg" alt="">
+        </section>
+        <section>
+            <img src="../assets/photos/kingfisher.jpg" alt="">
+        </section>
+        <section >
+            <img src="../assets/photos/owl.jpg" id="owl" alt="">
+        </section>
+        <section>
+            <img src="../assets/photos/turtle.jpg" id="turtle" alt="">
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .body {
+        min-height: 500px;
+    }
+    img {
+        margin: 30px;
+        height: 200px;
+        transition: all 1s ease-in-out;
+    }
+    img:hover {
+        transform: scale(1.5);
+
+    }
+    /*#owl {
+        height: 227px;
+    }
+    #turtle {
+        height: 190px;
+    }
+    div {
+        display: inline-block;
+    }*/
+    section {
+        display: inline-block;
+    }
+</style>
